001f85479879cfb9fdbb5b814e7b7be765754893
DRELLICHE <drelliche@chop.edu>
2022-07-29 12:37:48 -0400

    add comments, remove sample module from table

--- assets/scripts/create_table_of_contents.sh
+++ assets/scripts/create_table_of_contents.sh
@@ -18,18 +18,18 @@ done
 echo $HEADINGS > table_of_contents.md
 echo $BREAK >> table_of_contents.md
 
-# Create entries
+# Create entries, with links to the liascript course and the .md files
 
 for FOLDER in *
 do
-  if [[ -s $FOLDER/$FOLDER.md ]]        ## Only do this for folders that have a course .md file inside an identically named folder in education_modules
+  if [[ -s $FOLDER/$FOLDER.md && "$FOLDER" != "a_sample_module_template" ]]      ## Only do this for folders that have a course .md file inside an identically named folder in education_modules
     then
       ROW='|'
       ROW+=" [Training Course](https://liascript.github.io/course/?https://raw.githubusercontent.com/arcus/education_modules/main/$FOLDER/$FOLDER.md) |"
       ROW+=" [Markdown File](https://github.com/arcus/education_modules/blob/main/$FOLDER/$FOLDER.md) |"
       for COL in $COLUMNS
         do
-          ROW+=" `grep -m 1 $COL: $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]'` |"
+          ROW+=" `grep -m 1 $COL: $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]'` |"  # Pull the YAML entry but remove excess white space at the front, as well as any unprintable characters
         done
       echo $ROW >> table_of_contents.md
   fi
