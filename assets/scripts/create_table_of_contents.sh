e842d2ac7733bf8bcaf0d32732e32fb8ee6d0819
DRELLICHE <drelliche@chop.edu>
2022-09-07 14:22:43 -0400

    add site_table_of_contents

--- assets/scripts/create_table_of_contents.sh
+++ assets/scripts/create_table_of_contents.sh
@@ -3,8 +3,10 @@
 # This script should be run from the main education_modules directory
 # With the command:
 # bash assets/scripts/create_table_of_contents.sh
-# It will create a .md file called table_of_contents.md
-# It will also create a .csv called table_of_contents.csv
+# It will create 3 files:
+#   table_of_contents.md for our reference
+#   table_of_contents.csv for analysis
+#   site_table_of_contents.md for display on our public facing website
 
 #############################
 # First we create the .md file
@@ -71,5 +73,43 @@ do
           ROW+=" `grep -m 1 $COL: $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]' | tr ',' ' '` ,"  # Pull the YAML entry but remove excess white space at the front, as well as any unprintable characters or commas
         done
       echo $ROW >> table_of_contents.csv
+  fi
+done
+
+
+#############################
+# This is an additional table of contents for use on our public facing website for learners and educators, not potential developers.
+#############################
+
+COLUMNS="title comment estimated_time"
+
+# Create a .md file with the column headings specified above
+
+HEADINGS='| Training Course | Description | Estimated Time |'
+BREAK='|'
+for COL in $COLUMNS
+do
+  BREAK+=" -- |"
+done
+
+echo $HEADINGS > site_table_of_contents.md
+echo $BREAK >> site_table_of_contents.md
+
+# Create entries, with links to the liascript course and the .md files
+
+for FOLDER in *
+do
+  if [[ -s $FOLDER/$FOLDER.md && "$FOLDER" != "a_sample_module_template"
+]]      ## Only do this for folders that have a course .md file inside an identically named folder in education_modules
+    then
+      ROW='|'
+      #create title as link to LiaScript
+          ROW+=" [`grep -m 1 "title:" $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]'`](https://liascript.github.io/course/?https://raw.githubusercontent.com/arcus/education_modules/main/$FOLDER/$FOLDER.md) |"  # Pull the YAML entry but remove excess white space at the front, as well as any unprintable characters
+      #Add description
+          ROW+=" `grep -m 1 "comment:" $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]'` |"  # Pull the YAML entry but remove excess white space at the front, as well as any unprintable characters
+      #Add estimated time
+          ROW+=" `grep -m 1 "estimated_time:" $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]'` |"  # Pull the YAML entry but remove excess white space at the front, as well as any unprintable characters
+      #  done
+      echo $ROW >> site_table_of_contents.md
   fi
 done
