4a72f1bf33257892b11426639c291a52dd0e1ec8
DRELLICHE <drelliche@chop.edu>
2023-04-12 09:43:04 -0400

    show comment when node is selected

--- knowledge_graphing/app.py
+++ knowledge_graphing/app.py
@@ -1,4 +1,5 @@
 from dash import Dash, html, Input, Output, dcc
+import dash_bootstrap_components as dbc
 import dash_cytoscape as cyto
 
 app = Dash(__name__)
@@ -13,53 +14,54 @@ styles = {
 
 nodes = [
     {
-        'data': {'id': short, 'title': title, 'author': author, 'time': time},
+        'data': {'id': short, 'title': title, 'author': author, 'time': time, 'comment': comment},
         ### Use classes here for all fo the data!!! 'classes': author
         #'position': {'x': 20*lat, 'y': -20*long}
     }
-    for short, title, author, time in (
-('bash_103_combining_commands', 'Bash: Combining Commands', 'Elizabeth Drellich and Nicole Feldman', '30 minutes'),
-('bash_command_line_101', 'Bash / Command Line 101', 'Nicole Feldman and Elizabeth Drellich', '40 minutes'),
-('bash_command_line_102', 'Bash: Searching and Organizing Files', 'Nicole Feldman and Elizabeth Drellich', '30 minutes'),
-('bash_conditionals_loops', 'Bash: Conditionals and Loops', 'Elizabeth Drellich', '1 hour'),
-('bash_scripts', 'Bash: Reusable Scripts', 'Elizabeth Drellich', '1 hour'),
-('citizen_science', 'Citizen Science', 'Rose Hartman', '45 minutes'),
-('data_management_basics', 'Research Data Management Basics', 'Ene Belleh', '40 minutes'),
-('data_storage_models', 'Types of Data Storage Solutions', 'Nicole Feldman', ''),
-('data_visualization_in_ggplot2', 'Data Visualization in ggplot2', 'Rose Hartman', '60 min'),
-('data_visualization_in_open_source_software', 'Data Visualization in Open Source Software', 'Rose Hartman', '20 minutes'),
-('data_visualization_in_seaborn', 'Data Visualization in seaborn', 'Rose Hartman', '1 hour'),
-('database_normalization', 'Database Normalization', 'Joy Payton', '40 minutes'),
-('demystifying_geospatial_data', 'Demystifying Geospatial Data', 'Elizabeth Drellich', '15 minutes'),
-('demystifying_python', 'Demystifying Python', 'Meredith Lee', '20m'),
-('demystifying_sql', 'Demystifying SQL', 'Peter Camacho', '40 minutes'),
-('directories_and_file_paths', 'Directories and File Paths', 'Meredith Lee', '15m'),
-('elements_of_maps', 'The Elements of Maps', 'Elizabeth Drellich', '45 minutes'),
-('geocode_lat_long', 'Encoding Geospatial Data: Latitude and Longitude', 'Elizabeth Drellich', '15 minutes'),
-('git_creation_and_tracking', 'Creating a Git Repository', 'Elizabeth Drellich', '1 hour'),
-('git_history_of_project', 'Exploring the History of your Git Repository', 'Elizabeth Drellich', '30 minutes'),
-('git_intro', 'Intro to Version Control', 'Rose Hartman', '10 min'),
-('git_setup_mac_and_linux', 'Setting Up Git on Mac and Linux', 'Rose Hartman', '15 min'),
-('git_setup_windows', 'Setting Up Git on Windows', 'Elizabeth Drellich', '25 min'),
-('how_to_troubleshoot', 'How to Troubleshoot', 'Joy Payton', '30 minutes'),
-('learning_to_learn', 'Learning to Learn Data Science', 'Rose Franzen', '20 minutes'),
-('omics_orientation', 'Omics Orientation', 'Meredith Lee', '15m'),
-('pandas_transform', 'Transform Data with pandas', 'Elizabeth Drellich', '1 hour'),
-('python_basics_writing_python_code', 'Python Basics: Writing Python Code', 'Meredith Lee', '1 hour'),
-('python_practice', 'Python Practice', 'Meredith Lee', '1 hour'),
-('r_basics_introduction', 'R Basics: Introduction', 'Joy Payton', '1 hour'),
-('r_basics_transform_data', 'R Basics: Transforming Data With dplyr', 'Joy Payton', '1 hour'),
-('r_basics_visualize_data', 'R Basics: Visualizing Data With ggplot2', 'Joy Payton', '1 hour'),
-('r_missing_values', 'Missing Values in R', 'Rose Hartman', '45 min'),
-('r_practice', 'R Practice', 'Meredith Lee', '1 hour'),
-('r_reshape_long_wide', 'Reshaping Data in R: Long and Wide Data', 'Joy Payton', '1 hour'),
-('reproducibility', 'Reproducibility, Generalizability, and Reuse', 'Joy Payton', '1 hour'),
-('sql_basics', 'SQL Basics', 'Peter Camacho', '1 hour'),
-('sql_intermediate', 'SQL, Intermediate Level', 'Peter Camacho', '1 hour'),
-('sql_joins', 'SQL Joins', 'Joy Payton', '1 hour'),
-('statistical_tests', 'Statistical Tests in Open Source Software', 'Rose Hartman', '20 minutes (although reading through the linked tutorials may take much longer, depending on which tests you choose to read about)'),
-('tidy_data', 'Tidy Data', 'Joy Payton', '45 minutes'),
-('using_redcap_api', 'Using the REDCap API', 'Joy Payton', '1 hour')
+    for short, title, author, time, comment in (
+('bash_103_combining_commands', 'Bash: Combining Commands', 'Elizabeth Drellich and Nicole Feldman', '30 minutes','This module will teach you how to combine two or more commands in bash to create more complicated pipelines in Bash.'),
+('bash_command_line_101', 'Bash / Command Line 101', 'Nicole Feldman and Elizabeth Drellich', '40 minutes','This course teaches learners to navigate their computer, as well as view and edit files, from the command line using Bash.'),
+('bash_command_line_102', 'Bash: Searching and Organizing Files', 'Nicole Feldman and Elizabeth Drellich', '30 minutes','This module will teach you how to use the bash shell to search and organize your files.'),
+('bash_conditionals_loops', 'Bash: Conditionals and Loops', 'Elizabeth Drellich', '1 hour','This module teaches you how to iterate through "for" loops and write conditional statements in Bash.'),
+('bash_scripts', 'Bash: Reusable Scripts', 'Elizabeth Drellich', '1 hour','This module will teach you how to create and use simple Bash scripts to make repetitive tasks as simple as possible. '),
+('citizen_science', 'Citizen Science', 'Rose Hartman', '45 minutes','This is an overview of citizen science for biomedical researchers.'),
+('data_management_basics', 'Research Data Management Basics', 'Ene Belleh', '40 minutes','Learn the basics about research data management.'),
+('data_storage_models', 'Types of Data Storage Solutions', 'Nicole Feldman', '','This course will focus on different data storage solutions available to an end user and the unique characteristics of each type. This course will also cover how each storage type impacts one\'s access to data and computing capabilities.'),
+('data_visualization_in_ggplot2', 'Data Visualization in ggplot2', 'Rose Hartman', '60 min','This module includes code and explanations for several popular data visualizations, using R\'s ggplot2 package. It also includes examples of how to modify ggplot2 plots to customize them for different uses (e.g. adhering to journal requirements for visualizations).'),
+('data_visualization_in_open_source_software', 'Data Visualization in Open Source Software', 'Rose Hartman', '20 minutes','Introduction to principles of data vizualization and typical data vizualization workflows using two common open source libraries: ggplot2 and seaborn.'),
+('data_visualization_in_seaborn', 'Data Visualization in seaborn', 'Rose Hartman', '1 hour','This module includes code and explanations for several popular data visualizations using python\'s seaborn library. It also includes examples of how to modify seaborn plots to customize them for different uses.  '),
+('database_normalization', 'Database Normalization', 'Joy Payton', '40 minutes','Learn about the concept of normalization and why it\'s important for organizing complicated data in relational databases.'),
+('demystifying_geospatial_data', 'Demystifying Geospatial Data', 'Elizabeth Drellich', '15 minutes','This module is a brief introduction to geospatial (location) data.'),
+('demystifying_python', 'Demystifying Python', 'Meredith Lee', '20m','This module introduces the Python programming language, explores why Python is useful in research, and describes how to download Python and Jupyter.'),
+('demystifying_sql', 'Demystifying SQL', 'Peter Camacho', '40 minutes','SQL is a relational database solution that has been around for decades.  Learn more about this technology at a high level, without having to write code.'),
+('directories_and_file_paths', 'Directories and File Paths', 'Meredith Lee', '15m','In this module, learners will explore what a directory is and how to describe the location of a file using its file path.   '),
+('elements_of_maps', 'The Elements of Maps', 'Elizabeth Drellich', '45 minutes','This is a general overview of ways that geospatial data can be communicated visually using maps.'),
+('geocode_lat_long', 'Encoding Geospatial Data: Latitude and Longitude', 'Elizabeth Drellich', '15 minutes','This is an introduction to latitude and longitude and the importance of geocoding - encoding geospatial data in the coordinate system.'),
+('git_creation_and_tracking', 'Creating a Git Repository', 'Elizabeth Drellich', '1 hour','Create a new Git repository and get started with version control.'),
+('git_history_of_project', 'Exploring the History of your Git Repository', 'Elizabeth Drellich', '30 minutes','This module will teach you how to look at past versions of your work on Git and compare your project with previous versions.'),
+('git_intro', 'Intro to Version Control', 'Rose Hartman', '10 min','An introduction to what version control systems do and why you might want to use one.'),
+('git_setup_mac_and_linux', 'Setting Up Git on Mac and Linux', 'Rose Hartman', '15 min','This module provides recommendations and examples to help new users configure git on their computer for the first time on a Mac or Linux computer.'),
+('git_setup_windows', 'Setting Up Git on Windows', 'Elizabeth Drellich', '25 min','This module provides recommendations and examples to help new users configure Git on their Windows computer for the first time.'),
+('how_to_troubleshoot', 'How to Troubleshoot', 'Joy Payton', '30 minutes','Learning to use technical methods like coding and version control in your research inevitably means running into problems.  Learn practical methods for troubleshooting and moving past error codes and other difficulties.'),
+('learning_to_learn', 'Learning to Learn Data Science', 'Rose Franzen', '20 minutes','Discover how learning data science is different than learning other subjects.'),
+('omics_orientation', 'Omics Orientation', 'Meredith Lee', '15m','This module provides a brief introduction to omics and its associated fields.'),
+('pandas_transform', 'Transform Data with pandas', 'Elizabeth Drellich', '1 hour','This is an introduction to transforming data using a Python library named pandas.'),
+('python_basics_writing_python_code', 'Python Basics: Writing Python Code', 'Meredith Lee', '1 hour','Learn the foundations of writing Python code.'),
+('python_practice', 'Python Practice', 'Meredith Lee', '1 hour','Use the basics of Python coding, data transformation, and data visualization to work with real data. '),
+('r_basics_introduction', 'R Basics: Introduction', 'Joy Payton', '1 hour','Introduction to R and hands-on first steps for brand new beginners.'),
+('r_basics_transform_data', 'R Basics: Transforming Data With dplyr', 'Joy Payton', '1 hour','Learn how to transform (or wrangle) data using R\'s `dplyr` package.'),
+('r_basics_visualize_data', 'R Basics: Visualizing Data With ggplot2', 'Joy Payton', '1 hour','Learn how to visualize data using R\'s `ggplot2` package.'),
+('r_missing_values', 'Missing Values in R', 'Rose Hartman', '45 min','A practical demonstration of how missing values show up in R and how to deal with them. Note that this module does **not** cover statistical approaches for handling missing data, but instead focuses on the code you need to find, work with, and assign missing values in R.'),
+('r_practice', 'R Practice', 'Meredith Lee', '1 hour','Use the basics of R coding, data transformation, and data visualization to work with real data. '),
+('r_reshape_long_wide', 'Reshaping Data in R: Long and Wide Data', 'Joy Payton', '1 hour','A module that teaches how to reshape tabular data in R, concentrating on some typical shapes known as "long" and "wide" data.'),
+('reproducibility', 'Reproducibility, Generalizability, and Reuse', 'Joy Payton', '1 hour','This module provides learners with an approachable introduction to the concepts and impact of **research reproducibility**, **generalizability**, and **data reuse**, and how technical approaches can help make these goals more attainable.'),
+('sql_basics', 'SQL Basics', 'Peter Camacho', '1 hour','Structured Query Language, or SQL, is a relational database solution that has been around for decades.  Learn how to do basic SQL queries on single tables, by using code, hands-on.'),
+('sql_intermediate', 'SQL, Intermediate Level', 'Peter Camacho', '1 hour','Learn how to do intermediate SQL queries on single tables, by using code, hands-on.'),
+('sql_joins', 'SQL Joins', 'Joy Payton', '1 hour','Learn about SQL joins: what they accomplish, and how to write them.'),
+('statistical_tests', 'Statistical Tests in Open Source Software', 'Rose Hartman', '20 minutes (although reading through the linked tutorials may take much longer, depending on which tests you choose to read about)','This module provides an overview of the most commonly used kinds of statistical tests and links to code for running many of them in both R and python.'),
+('tidy_data', 'Tidy Data', 'Joy Payton', '45 minutes','Tidy is a technical term in data analysis and describes an optimal way for organizing data that will be analyzed computationally.'),
+('using_redcap_api', 'Using the REDCap API', 'Joy Payton', '1 hour','REDCap is a research data capture tool used by many researchers in basic, translational, and clinical research efforts.  Learn how to use the REDCap API in this module.')
+
 
     )
 ]
@@ -174,6 +176,8 @@ default_stylesheet = [
     }
 ]
 
+### Create a function that changes the stylesheet based on user inputs.
+
 def author_selected(author_name):
     ### Create a stylesheet based on the selected author
     select_author_modules = []
@@ -182,16 +186,16 @@ def author_selected(author_name):
     selection=str('[author *= "')+ str(author_name) + str('" ]')
     turned_on = {}
     turned_on['selector'] = selection
-    turned_on['style'] = {'shape': 'square', 'color':'#BFD7B5', 'label': 'data(title)'}
+    turned_on['style'] = {'shape': 'square', 'color':'#BFD7B5', 'label': 'data(title)', 'color': '#000000'}
     select_author_modules.append(turned_on)
 
     ### Turn off nodes not corresponding to that author
     selection=str('[author !*= "')+ str(author_name) + str('" ]')
     turned_off = {}
     turned_off['selector'] = selection
-    turned_off['style'] = {'shape': 'square', 'color':'#BFD7B5', 'opacity': 0.2}
+    turned_off['style'] = {'shape': 'square', 'color':'#BFD7B5', 'opacity': 0.3}
     select_author_modules.append(turned_off)
-    
+
     return select_author_modules
 
 
@@ -206,27 +210,19 @@ app.layout = html.Div([
         style={'width': '100%', 'height': '450px'}
     ),
     dcc.Dropdown(
-    id='dropdown-update-layout',
-    value='grid',
-    clearable=False,
-    options=[
-        {'label': name.capitalize(), 'value': name}
-        for name in ['grid', 'random', 'circle', 'cose', 'concentric']
-    ]
-),
-    dcc.Dropdown(
     id='author_selector',
-    value='Joy Payton',
-    clearable=False,
+    #value='Joy Payton',
+    clearable=True,
     options=[
         {'label': name, 'value': name}
         for name in ['Elizabeth Drellich', 'Joy Payton', 'Rose Franzen', 'Rose Hartman', 'Meredith Lee', 'Nicole Feldman', 'Ene Belleh', 'Peter Camacho']
     ]
 ),
-    dcc.Markdown(id='cytoscape-tapNodeData-output'),
-    html.P(id='cytoscape-tapEdgeData-output'),
-    html.P(id='cytoscape-mouseoverNodeData-output'),
-    html.P(id='cytoscape-mouseoverEdgeData-output'),
+
+    dcc.Markdown(id='selected_module_text'),
+    #html.P(id='cytoscape-tapEdgeData-output'),
+    #html.P(id='cytoscape-mouseoverNodeData-output'),
+    #html.P(id='cytoscape-mouseoverEdgeData-output'),
     
 ])
 
@@ -237,43 +233,34 @@ def update_author_selection(author_name):
     return author_selected(author_name)
 
 
-
-@app.callback(Output('module_visualization', 'layout'),
-              Input('dropdown-update-layout', 'value'))
-def update_layout(layout):
-    return {
-        'name': layout,
-        'animate': True
-    }
-
-@app.callback(Output('cytoscape-tapNodeData-output', 'children'),
+@app.callback(Output('selected_module_text', 'children'),
               Input('module_visualization', 'tapNodeData'))
 def displayTapNodeData(data):
     if data:
-        return "The selected module is [**" + data['title'] + "**](https://liascript.github.io/course/?https://raw.githubusercontent.com/arcus/education_modules/main/"+ data['id']+"/" +data['id'] + ".md) which was written by " + data['author'] +". We think it will take you about " +data['time']+"."
+        return "### [**" + data['title'] + "**](https://liascript.github.io/course/?https://raw.githubusercontent.com/arcus/education_modules/main/"+ data['id']+"/" +data['id'] + ".md) \n \n  By " + data['author'] +" \n \n Estimated length: " + data['time']+". \n \n" + data['comment'] + "\n #### Connected modules: \n "
 
 
-@app.callback(Output('cytoscape-tapEdgeData-output', 'children'),
-              Input('module_visualization', 'tapEdgeData'))
-def displayTapEdgeData(data):
-    if data:
-        return "You recently clicked/tapped the edge between " + \
-               data['source'].upper() + " and " + data['target'].upper()
+# @app.callback(Output('cytoscape-tapEdgeData-output', 'children'),
+#               Input('module_visualization', 'tapEdgeData'))
+# def displayTapEdgeData(data):
+#     if data:
+#         return "You recently clicked/tapped the edge between " + \
+#                data['source'].upper() + " and " + data['target'].upper()
 
 
-@app.callback(Output('cytoscape-mouseoverNodeData-output', 'children'),
-              Input('module_visualization', 'mouseoverNodeData'))
-def displayTapNodeData(data):
-    if data:
-        return "You recently hovered over the city: " + data['title']
+# @app.callback(Output('cytoscape-mouseoverNodeData-output', 'children'),
+#               Input('module_visualization', 'mouseoverNodeData'))
+# def displayTapNodeData(data):
+#     if data:
+#         return "You recently hovered over the city: " + data['title']
 
 
-@app.callback(Output('cytoscape-mouseoverEdgeData-output', 'children'),
-              Input('module_visualization', 'mouseoverEdgeData'))
-def displayTapEdgeData(data):
-    if data:
-        return "You recently hovered over the edge between " + \
-               data['source'].upper() + " and " + data['target'].upper()
+# @app.callback(Output('cytoscape-mouseoverEdgeData-output', 'children'),
+#               Input('module_visualization', 'mouseoverEdgeData'))
+# def displayTapEdgeData(data):
+#     if data:
+#         return "You recently hovered over the edge between " + \
+#                data['source'].upper() + " and " + data['target'].upper()
 
 
 if __name__ == '__main__':
--- knowledge_graphing/create_graph.sh
+++ knowledge_graphing/create_graph.sh
@@ -1,6 +1,7 @@
 ### This script will create the file knowledge_graph.py containing all of the possible data for visualizing connections between modules.
 
 graph_data=knowledge_graphing/graph_data.py
+node_data=knowledge_graphing/node_data.txt
 
 echo "import networkx as nx
 import matplotlib.pyplot as plt
@@ -10,6 +11,8 @@ G = nx.DiGraph()" > $graph_data
 
 ### Make every module a graph node
 
+echo "node_data = [" > $node_data
+
 for FOLDER in *
 do
   if [[ -s $FOLDER/$FOLDER.md && "$FOLDER" != "a_sample_module_template" ]]      ## Only do this for folders that have a course .md file inside an identically named folder in education_modules
@@ -17,11 +20,15 @@ do
       title="`grep -m 1 title: $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]'`"
       author="`grep -m 1 author: $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]'`"
       estimated_time="`grep -m 1 estimated_time: $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]'`"
-      create_node="G.add_node(\"$FOLDER\", title=\"$title\", author=\"$author\", estimated_time=\"$estimated_time\")"
-      echo $create_node >> $graph_data
-      echo "('$FOLDER', '$title', '$author', '$estimated_time'),"
+      comment="`grep -m 1 comment: $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]'`"
+      #create_node="G.add_node(\"$FOLDER\", title=\"$title\", author=\"$author\", estimated_time=\"$estimated_time\")"
+      #echo $create_node >> $graph_data
+      #echo $comment
+      echo "('$FOLDER', '$title', '$author', '$estimated_time','$comment')," >> $node_data
   fi
 done
+
+echo "]" >> $node_data
 
 ### Find all links to other modules:
 
