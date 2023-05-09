894d9f525e07439d4b9da8a81e10b9b52a02c450
DRELLICHE <drelliche@chop.edu>
2023-05-09 15:19:28 -0400

    automatically create node data with process_data.sh
    node data is now imported from module_data.py

--- knowledge_graphing/process_data.sh
+++ knowledge_graphing/process_data.sh
@@ -15,16 +15,24 @@ do
   if [[ -s $FOLDER/$FOLDER.md && "$FOLDER" != "a_sample_module_template" ]]      ## Only do this for folders that have a course .md file inside an identically named folder in education_modules
     then
       title="`grep -m 1 title: $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]'`"
-      echo "df.loc[\"$FOLDER\", \"Title\"] = \"$title\" " >> $graph_data
+      echo "df.loc[\"$FOLDER\", \"title\"] = \"$title\" " >> $graph_data
       author="`grep -m 1 author: $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]'`"
-      echo "df.loc[\"$FOLDER\", \"Author\"] = \"$author\" " >> $graph_data
+      echo "df.loc[\"$FOLDER\", \"author\"] = \"$author\" " >> $graph_data
       estimated_time="`grep -m 1 estimated_time: $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]'`"
-      echo "df.loc[\"$FOLDER\", \"Estimated Time\"] = \"$estimated_time\" " >> $graph_data
+      echo "df.loc[\"$FOLDER\", \"time\"] = \"$estimated_time\" " >> $graph_data
       #### Some comments and long descriptions contain double quotes... this is a problem. For the moment they have been replaced with the character +
       comment="`grep -m 1 comment: $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]' | tr '"' '+'`"
-      echo "df.loc[\"$FOLDER\", \"Comment\"] = \"$comment\" " >> $graph_data
+      echo "df.loc[\"$FOLDER\", \"comment\"] = \"$comment\" " >> $graph_data
       long_description="`grep -m 1 long_description: $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]' | tr '"' '+'`"
-      echo "df.loc[\"$FOLDER\", \"Long Description\"] = \"$long_description\" " >> $graph_data
+      echo "df.loc[\"$FOLDER\", \"long_description\"] = \"$long_description\" " >> $graph_data
+
+      ### Find the line number where the "@learning_objectives" first appears
+      start=$(( $(grep -n -m 1 "@learning_objectives" $FOLDER/$FOLDER.md  | cut -f1 -d:) +1 ))
+
+      end=$(( $(tail -n +$start $FOLDER/$FOLDER.md | grep -n -m 1 "@end" | cut -f1 -d:) - 1 ))
+
+      learning_objectives=$(tail -n +$start $FOLDER/$FOLDER.md | head -n $end | tr '\n' '&' | tr '"' '+')
+      echo "df.loc[\"$FOLDER\", \"Learning Objectives\"] = \"$learning_objectives\" " >> $graph_data
 
   fi
 done
