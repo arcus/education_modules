7409be6927aa0bb74996ee4faaeb62a558153edb
DRELLICHE <drelliche@chop.edu>
2023-05-10 16:50:23 -0400

    more metadata processed by shell script

--- assets/module_discovery_app/process_data.sh
+++ assets/module_discovery_app/process_data.sh
@@ -1,6 +1,7 @@
 ### This script will create the file knowledge_graph.py containing all of the possible data for visualizing connections between modules.
+### Run this script from the main education_modules directory
 
-graph_data=knowledge_graphing/module_data.py
+graph_data=assets/module_discovery_app/module_data.py
 
 echo "import pandas as pd
 df=pd.DataFrame()" > $graph_data
@@ -15,13 +16,36 @@ do
   if [[ -s $FOLDER/$FOLDER.md && "$FOLDER" != "a_sample_module_template" ]]      ## Only do this for folders that have a course .md file inside an identically named folder in education_modules
     then
       ### pull the one-line macros
-      title="`grep -m 1 title: $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]'`"
-      echo "df.loc[\"$FOLDER\", \"title\"] = \"$title\" " >> $graph_data
-      author="`grep -m 1 author: $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]'`"
-      echo "df.loc[\"$FOLDER\", \"author\"] = \"$author\" " >> $graph_data
-      estimated_time="`grep -m 1 estimated_time: $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]'`"
-      echo "df.loc[\"$FOLDER\", \"time\"] = \"$estimated_time\" " >> $graph_data
+      for CATEGORY in "title" "author" "estimated_time"
+      do
+        category_metadata="`grep -m 1 "$CATEGORY": $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]'`"
+        echo "df.loc[\"$FOLDER\", \"$CATEGORY\"] = \"$category_metadata\"" >> $graph_data
+
+        # title="`grep -m 1 title: $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]'`"
+        # echo "df.loc[\"$FOLDER\", \"title\"] = \"$title\" " >> $graph_data
+        # author="`grep -m 1 author: $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]'`"
+        # echo "df.loc[\"$FOLDER\", \"author\"] = \"$author\" " >> $graph_data
+        # estimated_time="`grep -m 1 estimated_time: $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]'`"
+        # echo "df.loc[\"$FOLDER\", \"time\"] = \"$estimated_time\" " >> $graph_data
+      done
+
+      ### good_first_module is not yet everywhere, but will be a required field 
+      if grep "good_first_module" -q $FOLDER/$FOLDER.md
+      then
+        good_first_module="`grep -m 1 good_first_module: $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]'`"
+      echo "df.loc[\"$FOLDER\", \"Good First Module\"] = \"$good_first_module\" " >> $graph_data
+      fi
       
+      ### Coding metadata and sequence metadata will always be in some modules but not others
+      for CATEGORY in "coding_required" "coding_language" "coding_level" "sequence_name" "next_sequential_module"
+      do
+        if grep "$CATEGORY" -q $FOLDER/$FOLDER.md
+        then
+          category_metadata="`grep -m 1 "$CATEGORY": $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]'`"
+        echo "df.loc[\"$FOLDER\", \"$CATEGORY\"] = \"$category_metadata\"" >> $graph_data
+        fi
+      done
+
       #### TODO Some comments and long descriptions contain double quotes... this is a problem. For the moment they have been replaced with the character +
       comment="`grep -m 1 comment: $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]' | tr '"' '+'`"
       echo "df.loc[\"$FOLDER\", \"comment\"] = \"$comment\" " >> $graph_data
@@ -32,30 +56,48 @@ do
       start=$(( $(grep -n -m 1 "@learning_objectives" $FOLDER/$FOLDER.md  | cut -f1 -d:) +1 ))
 
       end=$(( $(tail -n +$start $FOLDER/$FOLDER.md | grep -n -m 1 "@end" | cut -f1 -d:) - 1 ))
-      #### TODO
+      #### TODO figure out line breaks!
       learning_objectives=$(tail -n +$start $FOLDER/$FOLDER.md | head -n $end | tr '\n' '&' | tr '"' '+')
       echo "df.loc[\"$FOLDER\", \"Learning Objectives\"] = \"$learning_objectives\" " >> $graph_data
-      
-      ### pull derived data about what modules this module links to
-      # echo "df.loc[\"$FOLDER\", \"Linked Courses\"] = [] " >> $graph_data
-      # for LINKED_COURSE in *
-      # do
-      #   if [[ -s $LINKED_COURSE/$LINKED_COURSE.md && "$LINKED_COURSE" != "a_sample_module_template" && "$LINKED_COURSE" != "$FOLDER" ]] 
-      #   then
-      #     if [ "$(grep -c $LINKED_COURSE $FOLDER/$FOLDER.md)" -ge 1 ]
-      #       then
-      #         echo "df.loc[\"$FOLDER\", \"Linked Courses\"] = df.loc[\"$FOLDER\", \"Linked Courses\"].append($LINKED_COURSE)" >> $graph_data
-      #     fi
 
-      #   fi 
-      # done
+      #### pre_reqs (The "IF" is because not every module has the prereqs in this format yet...)
+      if grep "@pre_reqs" -q $FOLDER/$FOLDER.md
+      then
+        start=$(( $(grep -n -m 1 "@pre_reqs" $FOLDER/$FOLDER.md  | cut -f1 -d:) +1 ))
 
+        end=$(( $(tail -n +$start $FOLDER/$FOLDER.md | grep -n -m 1 "@end" | cut -f1 -d:) - 1 ))
+        #### TODO figure out line breaks!
+        pre_reqs=$(tail -n +$start $FOLDER/$FOLDER.md | head -n $end | tr '\n' '&' | tr '"' '+')
+        echo "df.loc[\"$FOLDER\", \"Prerequisties\"] = \"$pre_reqs\" " >> $graph_data
+      fi
+
+      #### sets_you_up_for (The "IF" is because not every module has this yet...)
+      if grep "@sets_you_up_for" -q $FOLDER/$FOLDER.md
+      then
+        start=$(( $(grep -n -m 1 "@sets_you_up_for" $FOLDER/$FOLDER.md  | cut -f1 -d:) +1 ))
+
+        end=$(( $(tail -n +$start $FOLDER/$FOLDER.md | grep -n -m 1 "@end" | cut -f1 -d:) - 1 ))
+        
+        sets_you_up_for=$(tail -n +$start $FOLDER/$FOLDER.md | head -n $end | tr '\n' ' ' | tr '-' ' ')
+        echo "df.loc[\"$FOLDER\", \"Sets You Up For\"] = \"$sets_you_up_for\" " >> $graph_data
+      fi
+
+      #### depends_on_knowledge_available_in (The "IF" is because not every module has this yet...)
+      if grep "@depends_on_knowledge_available_in" -q $FOLDER/$FOLDER.md
+      then
+        start=$(( $(grep -n -m 1 "@depends_on_knowledge_available_in" $FOLDER/$FOLDER.md  | cut -f1 -d:) +1 ))
+
+        end=$(( $(tail -n +$start $FOLDER/$FOLDER.md | grep -n -m 1 "@end" | cut -f1 -d:) - 1 ))
+        
+        depends_on_knowledge_available_in=$(tail -n +$start $FOLDER/$FOLDER.md | head -n $end | tr '\n' ' ' | tr '-' ' ')
+        echo "df.loc[\"$FOLDER\", \"Depends On Knowledge In\"] = \"$depends_on_knowledge_available_in\" " >> $graph_data
+      fi
   fi
 done
 
-echo  "df[\"Linked Courses\"] = [list() for x in range(len(df.index))]" >> $graph_data
-
 ### Find all links to other modules:
+
+echo  "df[\"Linked Courses\"] = [list() for x in range(len(df.index))]" >> $graph_data
 
 for FOLDER in *
 do
@@ -79,3 +121,9 @@ do
   fi
 done
 
+### Debugging code, modify as needed:
+
+#echo "print(df.loc[:,[\"coding_required\", \"coding_language\", \"coding_level\", \"sequence_name\", \"next_sequential_module\"]])">>$graph_data
+#
+#
+#python assets/module_discovery_app/module_data.py
