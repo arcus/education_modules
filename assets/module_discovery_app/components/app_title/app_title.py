7c1b5c24b69504b13db9fea180a66c3b22893d1a
DRELLICHE <drelliche@chop.edu>
2023-06-08 14:30:48 -0400

    remove "DART" from app title

--- assets/module_discovery_app/components/app_title/app_title.py
+++ assets/module_discovery_app/components/app_title/app_title.py
@@ -1,4 +1,4 @@
 from dash import Dash, html, dcc
 import dash_bootstrap_components as dbc
 
-app_title = dbc.Col(html.B(["DART Module Discovery Tool"]), style={'textAlign': 'center','font-size':'40px'}, align='end')
+app_title = dbc.Col(html.B(["Module Discovery Tool"]), style={'textAlign': 'center','font-size':'40px'}, align='end')
