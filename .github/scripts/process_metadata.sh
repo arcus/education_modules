c26db0e713598689f39066f6df81810abaa1650b
DRELLICHE <drelliche@chop.edu>
2023-08-23 14:56:38 -0400

    clean up block macro code

--- .github/scripts/process_metadata.sh
+++ .github/scripts/process_metadata.sh
@@ -11,62 +11,39 @@ echo >> $metadata_df
 
 for FOLDER in *
 do
-  if [[ -s $FOLDER/$FOLDER.md && "$FOLDER" ]]      ## Only do this for folders that have a course .md file inside an identically named folder in education_modules
+    if [[ -s $FOLDER/$FOLDER.md && "$FOLDER" ]]      ## Only do this for folders that have a course .md file inside an identically named folder in education_modules
     then
-      ### pull the one-line macros, process "comment" and "long_description" separately due to possible double quotes
-      for CATEGORY in  "author" "email" "version" "current_version_description" "module_type" "docs_version" "language" "narrator" "mode" "title" "estimated_time_in_minutes" "module_type" "good_first_module" "data_domain" "data_task" "coding_required" "coding_level" "coding_language" "sequence_name" "previous_sequential_module" #"comment" "long_description"
-      do
-        category_metadata="`grep -m 1 "$CATEGORY": $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]'`"
-        echo "df.loc[\"$FOLDER\", \"$CATEGORY\"] = \"$category_metadata\"" >> $metadata_df
-      done
+        ### pull the one-line macros, process "comment" and "long_description" separately due to possible double quotes
+        for CATEGORY in  "author" "email" "version" "current_version_description" "module_type" "docs_version" "language" "narrator" "mode" "title" "estimated_time_in_minutes" "module_type" "good_first_module" "data_domain" "data_task" "coding_required" "coding_level" "coding_language" "sequence_name" "previous_sequential_module" #"comment" "long_description"
+        do
+            category_metadata="`grep -m 1 "$CATEGORY": $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]'`"
+            echo "df.loc[\"$FOLDER\", \"$CATEGORY\"] = \"$category_metadata\"" >> $metadata_df
+        done
 
-      #### TODO Some comments and long descriptions contain double quotes... this is a problem. For the moment they have been replaced with the character +
-      comment="`grep -m 1 comment: $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]' | tr '"' '+'`"
-      echo "df.loc[\"$FOLDER\", \"comment\"] = \"$comment\" " >> $metadata_df
-      long_description="`grep -m 1 long_description: $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]' | tr '"' '+'`"
-      echo "df.loc[\"$FOLDER\", \"long_description\"] = \"$long_description\" " >> $metadata_df
-
-      ### Start pulling the data from block macros. So far there is only one of those. First find the line number where the "@learning_objectives" first appears
-      start=$(( $(grep -n -m 1 "@learning_objectives" $FOLDER/$FOLDER.md  | cut -f1 -d:) +1 ))
-
-      end=$(( $(tail -n +$start $FOLDER/$FOLDER.md | grep -n -m 1 "@end" | cut -f1 -d:) - 1 ))
-      #### TODO figure out line breaks!
-      learning_objectives=$(tail -n +$start $FOLDER/$FOLDER.md | head -n $end | tr '\n' '&' | tr '"' '+')
-      echo "df.loc[\"$FOLDER\", \"learning_objectives\"] = \"$learning_objectives\" " >> $metadata_df
-
-      #### pre_reqs (The "IF" is because not every module has the prereqs in this format yet...)
-      if grep "@pre_reqs" -q $FOLDER/$FOLDER.md
-      then
-        start=$(( $(grep -n -m 1 "@pre_reqs" $FOLDER/$FOLDER.md  | cut -f1 -d:) +1 ))
-
-        end=$(( $(tail -n +$start $FOLDER/$FOLDER.md | grep -n -m 1 "@end" | cut -f1 -d:) - 1 ))
-        #### TODO figure out line breaks!
-        pre_reqs=$(tail -n +$start $FOLDER/$FOLDER.md | head -n $end | tr '\n' '&' | tr '"' '+')
-        echo "df.loc[\"$FOLDER\", \"Prerequisties\"] = \"$pre_reqs\" " >> $metadata_df
-      fi
-
-      #### sets_you_up_for (The "IF" is because not every module has this yet...)
-      if grep "@sets_you_up_for" -q $FOLDER/$FOLDER.md
-      then
-        start=$(( $(grep -n -m 1 "@sets_you_up_for" $FOLDER/$FOLDER.md  | cut -f1 -d:) +1 ))
-
-        end=$(( $(tail -n +$start $FOLDER/$FOLDER.md | grep -n -m 1 "@end" | cut -f1 -d:) - 1 ))
+        #### TODO Some comments and long descriptions contain double quotes... this is a problem. For the moment they have been replaced with the character +
+      
+        comment="`grep -m 1 comment: $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]' | tr '"' '+'`"
+        echo "df.loc[\"$FOLDER\", \"comment\"] = \"$comment\" " >> $metadata_df
         
-        sets_you_up_for=$(tail -n +$start $FOLDER/$FOLDER.md | head -n $end | tr '\n' ' ' | tr '-' ' ')
-        echo "df.loc[\"$FOLDER\", \"Sets You Up For\"] = \"$sets_you_up_for\" " >> $metadata_df
-      fi
+        long_description="`grep -m 1 long_description: $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]' | tr '"' '+'`"
+        echo "df.loc[\"$FOLDER\", \"long_description\"] = \"$long_description\" " >> $metadata_df
 
-      #### depends_on_knowledge_available_in (The "IF" is because not every module has this yet...)
-      if grep "@depends_on_knowledge_available_in" -q $FOLDER/$FOLDER.md
-      then
-        start=$(( $(grep -n -m 1 "@depends_on_knowledge_available_in" $FOLDER/$FOLDER.md  | cut -f1 -d:) +1 ))
 
-        end=$(( $(tail -n +$start $FOLDER/$FOLDER.md | grep -n -m 1 "@end" | cut -f1 -d:) - 1 ))
-        
-        depends_on_knowledge_available_in=$(tail -n +$start $FOLDER/$FOLDER.md | head -n $end | tr '\n' ' ' | tr '-' ' ')
-        echo "df.loc[\"$FOLDER\", \"Depends On Knowledge In\"] = \"$depends_on_knowledge_available_in\" " >> $metadata_df
-      fi
-  fi
+        #### pull the block macros. To speed up the process because not all of these are necessarily present, there is an IF statement.
+
+        for BLOCK_MACRO in "@pre_reqs" "@learning_objectives" "sets_you_up_for" "depends_on_knowledge_in" "is_parallel_to" "version_history"
+        do
+            if grep $BLOCK_MACRO -q $FOLDER/$FOLDER.md
+            then
+                start=$(( $(grep -n -m 1 $BLOCK_MACRO $FOLDER/$FOLDER.md  | cut -f1 -d:) +1 ))
+
+                end=$(( $(tail -n +$start $FOLDER/$FOLDER.md | grep -n -m 1 "@end" | cut -f1 -d:) - 1 ))
+                #### TODO figure out a better solution to line breaks instead of just replacing them with & symbols!
+                macro_contents=$(tail -n +$start $FOLDER/$FOLDER.md | head -n $end | tr '\n' '&' | tr '"' '+')
+                echo "df.loc[\"$FOLDER\", \"$BLOCK_MACRO\"] = \"$macro_contents\" " >> $metadata_df
+            fi
+        done
+    fi
 done
 
 ### Find all links to other modules:
