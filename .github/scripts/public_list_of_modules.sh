eef8c0266a9c5b3893dd475ef8a1b87ff5fa9271
drelliche <99294374+drelliche@users.noreply.github.com>
2023-12-20 11:48:45 -0500

    Automate List of modules (#869)
    * Update public_list_of_modules.sh
    
    * first trial of website automation update
    
    * fix workflow dependencies
    
    * Update pull_metadata.yml
    
    * fix dependencies, test run
    
    * update metadata records
    
    * test pushing to public (hidden) website
    
    * test table width
    
    * force "something changed"
    
    * update metadata records
    
    * try resizing columns?
    
    * update metadata records
    
    * Update public_list_of_modules.sh
    
    * update metadata records
    
    * replace _ with space in collection column
    
    * prettify displayed metadata
    
    * point workflow to correct branches, files
    
    * Update bash_103_combining_commands.md
    
    ---------
    
    Co-authored-by: actions-user <actions@github.com>

--- .github/scripts/public_list_of_modules.sh
+++ .github/scripts/public_list_of_modules.sh
@@ -7,33 +7,60 @@ echo "---
 title: Educational Modules
 ---
 
-We understand that waiting for a cohort and guided curriculum might be too long to wait, so you are welcome to explore these materials on your own. The benefits of waiting and participating in our research include receiving a customized curriculum of materials and the peership and support of a community of learners like you. To express interest in participating in research, please fill out the [DART interest form](https://redcap.chop.edu/surveys/?s=FPHWFNEA9KN3HERF) or go directly to our [research screening form](https://redcap.link/DART-survey).
+This page includes a complete list of our published modules. 
 
-If you do access any of these modules, please fill out the module feedback survey at the end to let us know what you think." > $page
+We're also building a self-service tool to help you find the modules most relevant to you. 
+Test out our [prototype module discovery application](https://learn.arcus.chop.edu/), and please leave feedback to help us improve!
+
+" > $page
 
 # Start the table
-echo "| Training Course | Description | Estimated Time | Coding Language |
-| -- | -- | -- | -- |" >> $page
+echo "| Training Course | Description | Estimated Time | Collection | Coding Language | Task | Domain |
+| -- | -- | -- | -- | -- | -- | -- |" >> $page
 
 for FOLDER in *
 do
     if [[ -s $FOLDER/$FOLDER.md && "$FOLDER" ]]      ## Only do this for folders that have a course .md file inside an identically named folder in education_modules
     then
-        ### pull the one-line macros, process "comment" and "long_description" separately due to possible double quotes
-        # for CATEGORY in  "author" "email" "version" "current_version_description" "module_type" "docs_version" "language" "narrator" "mode" "title" "estimated_time_in_minutes" "module_type" "good_first_module" "data_domain" "data_task" "coding_required" "coding_level" "coding_language" "sequence_name" "previous_sequential_module" #"comment" "long_description"
-        # do
-        #     category_metadata="`grep -m 1 "$CATEGORY": $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]'`"
-        #     echo "df.loc[\"$FOLDER\", \"$CATEGORY\"] = \"$category_metadata\"" >> $metadata_df
-        # done
-        title="`grep -m 1 "title": $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]'`"
-        estimated_time_in_minutes="`grep -m 1 "estimated_time_in_minutes": $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]'`"
-        coding_language="`grep -m 1 "coding_language": $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]'`"
 
-        #### TODO Some comments and long descriptions contain double quotes... this is a problem. For the moment they have been replaced with the character +
-      
-        comment="`grep -m 1 comment: $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]' | tr '"' '+'`"
+        title="`grep -m 1 "title": $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]'`"
+
+        comment="`grep -m 1 comment: $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]'`"
+        comment=${comment//"\""/"\\\""} #add escape characters to quotes
         
-        echo "| [$title](https://liascript.github.io/course/?https://raw.githubusercontent.com/arcus/education_modules/main/$FOLDER/$FOLDER.md) | $comment | $estimated_time_in_minutes minutes| $coding_language |" >> $page
+        estimated_time_in_minutes="`grep -m 1 "estimated_time_in_minutes": $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]'`"
+        
+        collection="`grep -m 1 "collection": $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]'`"
+        collection=${collection//"_"/" "} #replace _ with spaces
+        ## Clunky way to make these look nice
+        collection=${collection//"demystifying"/"Demystifying"}
+        collection=${collection//"infrastructure"/"Infrastructure"}
+        collection=${collection//"statistics"/"Statistics"}
+        collection=${collection//"learn"/"Learn"}
+        collection=${collection//"machine"/"Machine"}
+        collection=${collection//"intro"/"Intro"}
+
+        coding_language="`grep -m 1 "coding_language": $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]'`"
+        coding_language=${coding_language//"bash"/"Bash"}
+        coding_language=${coding_language//"sql"/"SQL"}
+        coding_language=${coding_language//"python"/"Python"}
+        coding_language=${coding_language//"r"/"R"}
+        coding_language=${coding_language//"git"/"Git"}
+        
+                
+        task="`grep -m 1 "data_task": $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]'`"
+        task=${task//"_"/" "} #replace _ with spaces
+        task=${task//"data"/"Data"}
+
+        domain="`grep -m 1 "data_domain": $FOLDER/$FOLDER.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]'`"
+        domain=${domain//"ehr"/"EHR data"}
+        domain=${domain//"geospatial"/"Geospatial data"}
+        domain=${domain//"omics"/"Omics data"}
+        domain=${domain//"text"/"Text data"}
+        echo $domain
+
+        
+        echo "| [$title](https://liascript.github.io/course/?https://raw.githubusercontent.com/arcus/education_modules/main/$FOLDER/$FOLDER.md) | $comment | $estimated_time_in_minutes min| $collection | $coding_language | $task | $domain |" >> $page
 
     fi
 done
