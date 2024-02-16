06187ae54b8b1c65da1468cdb3327f7cae77d2f1
rosemm <hartmanr1@chop.edu>
2024-02-16 14:59:08 -0500

    Add R code used in module and missing image file

--- generalized_linear_regression/example_data.R
+++ generalized_linear_regression/example_data.R
@@ -1,3 +1,9 @@
+# show the relationship between probability, odds, and logits
+logit_demo <- data.frame(Logit = c(-Inf, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, Inf)) |> 
+  mutate(Odds = exp(Logit),
+         Probability = ifelse(Odds < Inf, Odds / (1 + Odds), 1))
+knitr::kable(logit_demo, digits = 3)
+
 library(tidyverse)
 # set the random seed so results replicate exactly with the random number generators
 set.seed(24601)
@@ -21,9 +27,13 @@ base_plot <- ggplot(data, aes(y=trisomy_21_dx, x=`BPD/FL`)) +
   labs(y = "Trisomy 21 Dx")
 
 # try plotting the data with just a linear model
-base_plot  + 
+base_plot + 
   stat_smooth(method = "lm")
 ggsave("linear_prediction.png", width = 5, height = 5, units = "in")
+
+base_plot + 
+  stat_smooth(method = "glm", method.args = list(family = "binomial"))
+ggsave("logit_prediction.png", width = 5, height = 5, units = "in")
 
 
 
